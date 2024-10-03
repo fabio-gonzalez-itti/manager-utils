1f946d6e12cf6ef1e2b3ef520e952e642ac2b8a3
fabio-gonzalez-itti-digital <fabio.gonzalez@itti.digital>
2024-10-03 11:13:59 -0400

    bump de versión.

--- manager-utils/setup.py
+++ manager-utils/setup.py
@@ -3,14 +3,12 @@ from setuptools import setup
 
 setup(
     name='manager-utils',
-    version='0.0.1',
+    version='0.0.2',
     description='Herramienta que permite simplificar operaciones relacionadas al despliegue de aplicaciones en entornos cloud.',
     url='https://github.com/fabio-gonzalez-itti/manager-utils',
     author='Fabio Antonio González Sosa',
     author_email='fabio.gonzalez@itti.digital',
     license='MIT',
     packages=['manager-utils'],
-    install_requires=[
-        'boto3==1.34.120'
-    ]
+    install_requires=[]
 )
